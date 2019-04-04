2785538e6c6eec24aa252b353efc526336b42001
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-03 22:41:48 -0500

    Add files via upload

---
+++ amusement_park.py
@@ -0,0 +1,15 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+#age = 3
+#age = 8
+age = 15
+
+if age < 4:
+	print("Admission is free!")
+elif age < 12:
+	print("Admission cost is $5.")
+else:
+	print("Admission cost is $10.")
+
