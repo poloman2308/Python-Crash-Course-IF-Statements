1eeeba53e189e8fd5ea99509d1ed9c8c7c2405d8
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-03 22:29:58 -0500

    Add files via upload

---
+++ conditional_tests.py
@@ -0,0 +1,18 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+car = 'audi'
+print("Is car == 'audi'? I say yes!")
+print(car == 'audi')
+
+print("\nIs car == 'toyota'? I say absolutely not!")
+print(car == 'bmw')
+
+motorcycle = 'ducati'
+print("\nIs a motorcycle == 'yamaha'? No it's not.")
+print(motorcycle == 'yamaha')
+
+print("\nIs my motorcycle == 'ducati'? Yes, it is!")
+print(motorcycle == 'ducati')
+
