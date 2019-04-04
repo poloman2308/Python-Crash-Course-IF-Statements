3aced131560afc875baa21f2af9874661d4b5570
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-03 22:20:39 -0500

    Add files via upload

---
+++ banned_user.py
@@ -0,0 +1,11 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+	print(user.title() + ", you can post a response if you wish.")
+
+	
