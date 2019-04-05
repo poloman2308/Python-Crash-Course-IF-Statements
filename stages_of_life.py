3d4b17556ac773d7ce60f8ffd36b6a97f854d146
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-04 22:40:23 -0500

    Add files via upload

---
+++ stages_of_life.py
@@ -0,0 +1,24 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+#age = 1
+#age = 3
+#age = 8
+#age = 16
+#age = 32
+age = 70
+
+if age < 2:
+	print("You are a baby.")
+elif age < 4:
+	print("You are a toddler.")
+elif age < 13:
+	print("You are a kid.")
+elif age < 20:
+	print("You are a teenager.")
+elif age < 65:
+	print("You are an adult.")
+else:
+	print("You are an elder.")
+
