a71dfd930f51130c7c5f73bf88d205a626ae0389
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-04 22:28:44 -0500

    Add files via upload

---
+++ alien_color2.py
@@ -0,0 +1,12 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+#alien_color = 'green'
+alien_color = 'red'
+
+if alien_color == 'green':
+	print("You just earned 5 points!")
+else:
+	print("You just earned 10 points!")
+
