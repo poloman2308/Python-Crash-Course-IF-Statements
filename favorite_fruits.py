3a8fe365c42672487fb65330e8d647be5578aa8f
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-04 22:49:08 -0500

    Add files via upload

---
+++ favorite_fruits.py
@@ -0,0 +1,15 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+favorite_fruits = ['apple', 'peach', 'banana']
+
+if 'apple' in favorite_fruits:
+	print("You really like apple's!")
+if 'peach' in favorite_fruits:
+	print("You really like peaches!")
+if 'banana' in favorite_fruits:
+	print("You really like banana's!")
+if 'orange' in favorite_fruits:
+	print("You really like oranges!")
+
